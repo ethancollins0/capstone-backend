dfaa184b69a3b4090951b974072d56f0e09464ae
Ethan Collins <ethantcollins98@gmail.com>
2019-10-15 10:22:40 -0600

    testing posts

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 
 
 app.post('/', (req, res) => {
-    res.json(`Posted ${req.body} to backend`)
+    res.json(`Posted ${req.body.name} and ${req.body.data} to backend`)
 })
 
 app.get('/', (req, res) => {
