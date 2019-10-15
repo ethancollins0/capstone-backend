f9879f964c16f8d71b8f1c6675994d14d7906e1d
Ethan Collins <ethantcollins98@gmail.com>
2019-10-15 11:08:35 -0600

    wip pi connection

--- index.js
+++ index.js
@@ -10,13 +10,46 @@ app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
 
-app.post('/', (req, res) => {
+app.post('/soil_data', (req, res) => {
     res.json(`Body: ${req.body.name} and Headers: ${req.headers.authorization} to backend`)
 })
 
 app.get('/', (req, res) => {
     res.json('worked get to backend')
 })
+
+app.post('/pi', (req, res) => {
+    req.body.email && req.body.pi_name
+        ? createToken(req.body.email, 1).then(token => res.json(token))
+        : res.json(null)
+})
+
+app.post('/livedata', validateToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET, (err, decoded) => {
+        err
+            ? res.json(err)
+            : res.json(decoded)
+    })
+})
+
+
+function createToken(email, pi_id){
+    return jwt.sign({ email, pi_id}, process.env.SECRET, {expiresIn: 60 * 60 * 24 * 30}, (err, token) => { // 30 days
+        return token
+    })
+}
+
+function validateToken(req, res, next){
+    const bearerHeader = req.headers['authorization'];
+        if (typeof bearerHeader == 'string'){
+            const bearer = bearerHeader.split(' ')
+            const bearerToken = bearer[1]
+            req.token = bearerToken
+            next();
+        } else {
+            res.json('forbidden')
+        }
+    }
 
 
 const port = process.env.PORT || 3002
