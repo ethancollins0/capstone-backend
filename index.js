f642dd16a892fbad36f5ff3128ef17d3458b0d8f
Ethan Collins <ethantcollins98@gmail.com>
2019-10-19 20:14:24 -0600

    wip

--- index.js
+++ index.js
@@ -52,7 +52,13 @@ io.on('connection', socket => {
             }
         }
         socket.on('disconnect', () => {
-
+            if (decoded.user_id && decoded.pi_id){
+                let clients = io.sockets.adapter.rooms[`${decoded.user_id}${decoded.pi_id}`]
+                io.in(`${decoded.user_id}${decoded.pi_id}`).emit('length', clients.length)
+            } else {
+                let clients = io.sockets.adapter.rooms[`${decoded.user_id}${socket.request._query.pi_id}`]
+                io.in(`${decoded.user_id}${socket.request._query.pi_id}`).emit('length', clients.length)
+            }
         })
 
         socket.on('water_data', (data) => {
