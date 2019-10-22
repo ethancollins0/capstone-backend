c1beca3400231176c5043105eab6b71a73fe9306
Ethan Collins <ethantcollins98@gmail.com>
2019-10-21 19:43:15 -0600

    mvp

--- seeds/02_systems.js
+++ seeds/02_systems.js
@@ -8,9 +8,9 @@ exports.seed = function(knex) {
         .then(users => {
           const user = users[0]
           return knex('systems').insert([
-            {name: "first pi", description: "moisture sensor", model: 'A+', user_id: user.id},
-            {name: "second pi", description: "garage door sensor", model: 'B+', user_id: user.id},
-            {name: "third pi", description: "thermostat", model: 'C+', user_id: user.id},
+            {name: "moisture", model: 'Pi 3 A+', user_id: user.id},
+            {name: "second pi", model: 'Pi 3 B+', user_id: user.id},
+            {name: "third pi", model: 'Pi 4 B', user_id: user.id},
           ]);
         })
       
