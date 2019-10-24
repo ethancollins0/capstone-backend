70673cd1db5a96907f88a335f927b60f01de98b4
Ethan Collins <ethantcollins98@gmail.com>
2019-10-24 13:37:58 -0600

    adds pictures for readme

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,6 @@ module.exports = {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`
   },
-  staging: {
-    client: 'pg',
-    connection: `${process.env.DATABASE_URL}?ssl=true`
-  },
   production: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`
